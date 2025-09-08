33557fc3a1220fde00105756d8dee4eaed88715d
Sweet~Zzx <3082269605@qq.com>
2025-09-08 21:42:15 +0800

    fix: 修复ESLint配置解决JSX作用域错误

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,34 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  env: { browser: true, es2022: true, node: true },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "prettier"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "detect" } },
-  rules: { "react/prop-types": "off" }
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      // 🔥 新增：JSX支持
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+  },
 };
