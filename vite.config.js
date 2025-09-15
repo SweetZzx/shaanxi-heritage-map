842f4a0033bd9bd77428d051895ae505ad7e24f2
Sweet~Zzx <3082269605@qq.com>
2025-09-15 14:57:41 +0800

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+
+// 获取当前环境变量以设置基础路径
+const base =
+  import.meta.env.VITE_GITHUB_REF === "refs/heads/main" ? "/Map-app/main/" : "/Map-app/onlyMap/";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,3 +14,5 @@ export default defineConfig({
     sourcemap: false
   }
 })
+
+
